fa08f9910581f30979f700da8a73c1c8877c92c0
Abhishek Tamrakar <abhishek.tamrakar08@gmail.com>
2017-12-20 19:16:58 +0530

    adding progress bars example 1

---
+++ progressbars/progress1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Author: Abhishek Tamrakar
+# Summary: proress bar 1
+# Type: With percentage completion
+##
+# version: 1.0.0
+#
+##################################################
+
+n=$1
+i=0
+calcPer()
+{
+ local c=$1
+ per=$(echo "scale=2;$c / $n * 100"|bc -l)
+# echo -n "[$per %]"
+}
+
+while [ $n -ge $i ]; do
+ calcPer $i
+ echo -en "checking status of $i/$n \t[${per%.*}%]\r"
+ sleep 2
+ echo $((i+=1)) >/dev/null
+done
+echo
