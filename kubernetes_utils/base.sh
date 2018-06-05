1c968414f1f9c5380f0049d9eda78f151c699749
Abhishek Tamrakar <abhishek.tamrakar08@gmail.com>
2018-06-06 00:27:53 +0530

    copyright added

--- kubernetes_utils/base.sh
+++ kubernetes_utils/base.sh
@@ -14,6 +14,8 @@
 #          You should have received a copy of the GNU General Public License
 #          along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
+# Copyright (C) 2018 Abhishek Tamrakar
+#
 KUBE=$(which kubectl)
 GET=$(which egrep)
 AWK=$(which awk)
